079c35f25835d5f2092d802676bf0fd3442dcf7f
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-10-21 23:33:59 -0400

    Fix typo

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,7 @@ import uuid
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    decription = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
