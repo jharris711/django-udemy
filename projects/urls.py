7796dbe56858cacd2e98e974ef5209e0a7d3473b
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-10-23 20:44:54 -0400

    Project CRUD/form.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from projects.views import projects, project, createProject
+from projects.views import projects, project, createProject, deleteProject, updateProject
 
 urlpatterns = [
     path('', projects, name="projects"),
     path('project/<str:pk>', project, name="project"),
     path('create-project/', createProject, name="create-project"),
+    path('update-project/<str:pk>', updateProject, name="update-project"),
+    path('delete-project/<str:pk>', deleteProject, name="delete-project"),
 ]
