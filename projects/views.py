d455170a64d4924f3914e6a2db76a9dbb1daa4fc
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-10-28 23:18:15 -0400

    Project crud from account page

--- projects/views.py
+++ projects/views.py
@@ -27,17 +27,22 @@ def project(request, pk):
 
 
 @login_required(login_url='login')
-def createProject(request): 
+def createProject(request):
+    profile = request.user.profile
     form = ProjectForm()
 
     if (request.method == 'POST'):
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+
             return redirect('projects')
 
     context = {
-        'form': form
+        'form': form,
+        'profile': profile
     }
     
     return render(request, 'projects/project_form.html', context)
@@ -45,12 +50,13 @@ def createProject(request):
 
 @login_required(login_url='login')
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
-
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
     if (request.method == 'POST'):
         form = ProjectForm(request.POST, request.FILES, instance=project)
+        
         if form.is_valid():
             form.save()
             return redirect('projects')
@@ -64,7 +70,8 @@ def updateProject(request, pk):
 
 @login_required(login_url='login')
 def deleteProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
 
     if (request.method == 'POST'):
         project.delete()
