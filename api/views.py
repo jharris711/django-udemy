e933eda2e710a9c53799a3db136781677a88452b
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-11-02 21:16:35 -0400

    Votes (Post reqs)

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from api.serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 
 @api_view(['GET'])
@@ -32,9 +32,30 @@ def getProjects(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request,pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+
+    project.getVoteCount
+
     serializer = ProjectSerializer(project, many=False)
 
     return Response(serializer.data)
